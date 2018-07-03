ae1ed3073a3400beccc76308ba3aca88607f988b
Picachu Wu <q8139q8139@gmail.com>
2018-07-03 20:41:30 +0800

    google map test

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Express = require('express');
+const fs = require('fs');
+
 //Express init.
 const app = Express();
-app.get('/', (res, req) =>{
+app.get('/', (req, res) =>{
     res.send('hello world');
 });
+app.use('/views', Express.static('views'));
 app.listen(process.env.PORT || 8080);
