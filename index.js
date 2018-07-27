837241c17ec2bc0cbe484c3a2a54af3f5f987a5c
Picachu Wu <q8139q8139@gmail.com>
2018-07-27 21:59:26 +0800

    try static template

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 //Express init.
 const app = Express();
 app.get('/', (req, res) =>{
-    res.send('hello world');
+    res.sendfile('./views/index.html');
 });
 app.get('/power',(req, res)=>{
     res.send((350+ Math.round(10 * Math.random())).toString());
